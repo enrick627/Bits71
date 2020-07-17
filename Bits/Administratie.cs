86b77e13a9b32895afb47bf54e97f48335e0efdc
enrick627 <enrick.demunter@student.romerocollege.be>
2020-07-17 08:54:44 +0200

    opnieuw begonnen

---
+++ Bits/Administratie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bits
+{
+    public class Administratie
+    {
+        /// <summary>
+        /// Alle werknemers
+        /// </summary>
+        public List<string> Werknemers()
+        {
+            List<string> Werknemer = new List<string>();
+            return Werknemer;
+        }
+
+        /// <summary>
+        /// Alle klanten
+        /// </summary>
+        /// <param name="Klant"></param>
+        public List<string> Klanten()
+        {
+            List<string> Klant = new List<string>();
+            return Klant;
+
+        }
+        /// <summary>
+        /// Voeg een klant toe aan de lijst
+        /// </summary>
+        /// <param name="Klanttoevoeg">de naam voor het toevoegen van klanten</param>
+        public void AddKlant(string Klanttoevoeg = Klanten)
+        {
+            
+        }
+        
+    }
+}
