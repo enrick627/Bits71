363551c985c15923241cd451b7326d758730eb88
enrick627 <enrick.demunter@student.romerocollege.be>
2020-07-17 09:31:45 +0200

    ik heb tot en met getklant gedaan

--- Bits/Administratie.cs
+++ Bits/Administratie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Bits
 {
@@ -35,6 +36,51 @@ namespace Bits
             List<string> Klant = new List<string>();
             Klant.Add("");
         }
-        
+        /// <summary>
+        /// hier worden de gegevens van de klant aangepast moet treu geven als lukt anders false
+        /// </summary>
+        public void Editklant()
+        {
+            List<string> Klant = new List<string>();
+            
+        }
+        /// <summary>
+        /// hier word de klant verwijdert
+        /// </summary>
+        public void Deleteklant()
+        {
+            List<string> Klant = new List<string>();
+            bool kandit = false;
+            if (kandit == false)
+            {
+                Klant.Remove("brooks koepka");
+                kandit = true;
+            }
+            else
+            {
+                kandit = false;
+
+            }
+            
+            
+        }
+        /// <summary>
+        /// klant opzoeken in lijst
+        /// </summary>
+        public Guid Getklant() 
+        {
+            List<string> Klant = new List<string>();
+            bool kandit = true;
+            if (kandit == true)
+            {
+                Klant.Contains("Rory Mcilroy");
+                kandit = true;
+            }
+            else
+            {
+                kandit = false;
+                MessageBox.Show("Klant werd niet gevonden");
+            }
+        }
     }
 }
